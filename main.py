f7c533b2b29f764d26f71abc2a146603ea4f963a
bevane <bevane@live.com>
2025-01-27 13:09:24 +0700

    feat: limit fps to 60

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from constants import *
 
 
 def main():
-    pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,9 @@ def main():
                 return
         screen.fill(color="black")
         pygame.display.flip()
+        # set framerate to 60 and save time that has passed since last call to
+        # tick to dt in milliseconds
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
